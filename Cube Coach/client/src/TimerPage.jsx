e8f3322f43c88d3aa773ebe5a9eed47edf103480
Thomas-Bevan <thomasbevan296@gmail.com>
2026-04-20 18:30:39 +0100

    added solve list and ao5

--- Cube Coach/client/src/TimerPage.jsx
+++ Cube Coach/client/src/TimerPage.jsx
@@ -4,6 +4,7 @@ function TimerPage() {
     const [time, setTime] = useState(0);
     const [running, setRunning] = useState(false);
     const [scramble, setScramble] = useState("R U R' U'");
+    const [solves, setSolves] = useState([]);
 
     useEffect(() => {
         let interval;
@@ -22,18 +23,39 @@ function TimerPage() {
     const handleKeyDown = (e) => {
         if (e.code === "Space") {
             e.preventDefault();
-            setRunning((prev) => !prev);
+
+            if (running) {
+                setRunning(false);
+
+                if (time > 0) {
+                    setSolves((prev) => [time, ...prev]);
+                    setTime(0);
+                }
+            } else {
+                setRunning(true);
+            }
+            
         }
     };
 
     useEffect(() => {
         window.addEventListener("keydown", handleKeyDown);
         return () => window.removeEventListener("keydown", handleKeyDown);
-    }, []);
+    }, [running, time]);
 
     const formatTime = (ms) => {
         const seconds = (ms / 1000).toFixed(2);
         return seconds;
+    };
+
+    const calculateAo5 = () => {
+        if (solves.length < 5) return null;
+
+        const last5 = solves.slice(0, 5);
+        const sorted = [...last5].sort((a, b) => a - b);
+        const middle = sorted.slice(1, 4);
+        const average = middle.reduce((a, b) => a + b, 0) / middle.length;
+        return average;
     };
 
    // useEffect(() => {
@@ -45,7 +67,22 @@ function TimerPage() {
             <p style={styles.scramble}>{scramble}</p>
             <h1 style={styles.timer}>{formatTime(time)}</h1>
             <p>Press SPACE to start/stop</p>
+            <p style={styles.average}>
+                Ao5: {calculateAo5() ? formatTime(calculateAo5()) : " - " }
+            </p>
+
+            <div style={styles.solveList}>
+                <h3>Solves</h3>
+                {solves.map((solve, index) => (
+                    <p key={index}>
+                        {index + 1}. {formatTime(solve)}
+                    </p>
+                ))}
+            </div>
         </div>
+
+     
+       
     );
 }
 
@@ -70,6 +107,19 @@ const styles = {
         letterSpacing: "2px",
         color: "F9FAFB"
     },
+    solveList: {
+        position: "absolute",
+        right: "40px",
+        top: "100px",
+        textAlign: "right",
+        maxHeight: "70vh",
+        overflowY: "auto",
+        color: "#9CA3AF"
+    },
+    average: {
+        marginTop: "10px",
+        color: "#9CA3AF"
+    }
 };
 
 export default TimerPage;
