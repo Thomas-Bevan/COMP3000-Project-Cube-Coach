35b1d2865d3e27e14c0052cba45dfab4ce222813
Thomas-Bevan <thomasbevan296@gmail.com>
2026-04-23 20:31:32 +0100

    added draw scramble

--- Cube Coach/client/src/TimerPage.jsx
+++ Cube Coach/client/src/TimerPage.jsx
@@ -5,6 +5,7 @@ function TimerPage() {
     const [running, setRunning] = useState(false);
     const [scramble, setScramble] = useState("R U R' U'");
     const [solves, setSolves] = useState([]);
+    
 
     useEffect(() => {
         let interval;
@@ -122,10 +123,162 @@ function TimerPage() {
     }
 
 
+    const createSolvedCube = () => ({
+        U: Array(9).fill("white"),
+        D: Array(9).fill("yellow"),
+        F: Array(9).fill("green"),
+        B: Array(9).fill("blue"),
+        L: Array(9).fill("orange"),
+        R: Array(9).fill("red"),
+    });
+
+    const rotateFace = (face) => {
+        let buffer;
+
+        // rotate face
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
+
+        // corners
+        buffer = face[0]
+        face[0] = face[6];
+        face[6] = face[8];
+        face[8] = face[2];
+        face[2] = buffer;
+
+        // edges
+        buffer = face[1]
+        face[1] = face[3];
+        face[3] = face[7];
+        face[7] = face[5];
+        face[5] = buffer;
+
+        return face;
+    }
+
+    const doU = (cube) => {
+
+        let buffer;
+
+        cube.U = rotateFace(cube.U);
+
+        // move bars
+        buffer = [cube.F[0], cube.F[1], cube.F[2]];
+        [cube.F[0], cube.F[1], cube.F[2]] = [cube.R[0], cube.R[1], cube.R[2]];
+        [cube.R[0], cube.R[1], cube.R[2]] = [cube.B[0], cube.B[1], cube.B[2]];
+        [cube.B[0], cube.B[1], cube.B[2]] = [cube.L[0], cube.L[1], cube.L[2]];
+        [cube.L[0], cube.L[1], cube.L[2]] = buffer;
+
+        return cube;
+
+    };
+
+    const doD = (cube) => {
+        let buffer;
+
+        cube.D = rotateFace(cube.D);
+
+        buffer = [cube.F[6], cube.F[7], cube.F[8]];
+        [cube.F[6], cube.F[7], cube.F[8]] = [cube.L[6], cube.L[7], cube.L[8]];
+        [cube.L[6], cube.L[7], cube.L[8]] = [cube.B[6], cube.B[7], cube.B[8]];
+        [cube.B[6], cube.B[7], cube.B[8]] = [cube.R[6], cube.R[7], cube.R[8]];
+        [cube.R[6], cube.R[7], cube.R[8]] = buffer;
+
+        return cube;
+    };
+
+    const doR = (cube) => {
+        let buffer;
+
+        cube.R = rotateFace(cube.R);
+
+        buffer = [cube.U[2], cube.U[5], cube.U[8]];
+        [cube.U[2], cube.U[5], cube.U[8]] = [cube.F[2], cube.F[5], cube.F[8]];
+        [cube.F[2], cube.F[5], cube.F[8]] = [cube.D[2], cube.D[5], cube.D[8]];
+        [cube.D[2], cube.D[5], cube.D[8]] = [cube.B[6], cube.B[3], cube.B[0]];
+        [cube.B[6], cube.B[3], cube.B[0]] = buffer;
+
+        return cube;
+    };
+
+    const doL = (cube) => {
+        let buffer;
+
+        cube.L = rotateFace(cube.L);
+
+        buffer = [cube.U[0], cube.U[3], cube.U[6]];
+        [cube.U[0], cube.U[3], cube.U[6]] = [cube.B[8], cube.B[5], cube.B[2]];
+        [cube.B[8], cube.B[5], cube.B[2]] = [cube.D[0], cube.D[3], cube.D[6]];
+        [cube.D[0], cube.D[3], cube.D[6]] = [cube.F[0], cube.F[3], cube.F[6]];
+        [cube.F[0], cube.F[3], cube.F[6]] = buffer;
+
+        return cube;
+    };
+
+    const doF = (cube) => {
+        let buffer;
+
+        cube.F = rotateFace(cube.F);
+
+        buffer = [cube.U[6], cube.U[7], cube.U[8]];
+        [cube.U[6], cube.U[7], cube.U[8]] = [cube.L[8], cube.L[5], cube.L[2]];
+        [cube.L[8], cube.L[5], cube.L[2]] = [cube.D[2], cube.D[1], cube.D[0]];
+        [cube.D[2], cube.D[1], cube.D[0]] = [cube.R[0], cube.R[3], cube.R[6]];
+        [cube.R[0], cube.R[3], cube.R[6]] = buffer;
+
+        return cube;
+    };
+
+    const doB = (cube) => {
+        let buffer;
+
+        cube.B = rotateFace(cube.B);
+
+        buffer = [cube.U[0], cube.U[1], cube.U[2]];
+        [cube.U[0], cube.U[1], cube.U[2]] = [cube.R[2], cube.R[5], cube.R[8]];
+        [cube.R[2], cube.R[5], cube.R[8]] = [cube.D[8], cube.D[7], cube.D[6]];
+        [cube.D[8], cube.D[7], cube.D[6]] = [cube.L[6], cube.L[3], cube.L[0]];
+        [cube.L[6], cube.L[3], cube.L[0]] = buffer;
+
+        return cube;
+    };
+
+
+    const doScramble = (scramble) => {
+        let cube = createSolvedCube();
+        //scramble = "R2 F2 R2"
+        const moves = scramble.split(" ");
+
+        moves.forEach((move) => {
+            let repeats;
+            if (move.length === 1) { repeats = 1 }
+            else if (move[1] === "'") { repeats = 3 }
+            else (repeats = 2);
+            console.log("repeats = " + repeats);
+            for (let i = 0; i < repeats; i++) {
+                if (move[0] === "U") cube = doU(cube);
+                if (move[0] === "D") cube = doD(cube);
+                if (move[0] === "R") cube = doR(cube);
+                if (move[0] === "L") cube = doL(cube);
+                if (move[0] === "F") cube = doF(cube);
+                if (move[0] === "B") cube = doB(cube);
+            };
+        });
+
+        return cube;
+    }
+
+    const [cubeState, setCubeState] = useState(createSolvedCube());
 
     useEffect(() => {
         setScramble(generateScramble());
     }, [])
+
+    useEffect(() => {
+        const newCube = doScramble(scramble);
+        setCubeState(newCube);
+    }, [scramble]);
 
     const ao5 = calculateAo5();
 
@@ -156,6 +309,45 @@ function TimerPage() {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div style={styles.cube}>
+
+                <div style={{ ...styles.face, gridColumn: "2", gridRow: "1" }}>
+                    {cubeState.U.map((color, i) => (
+                        <div key={i} style={{ ...styles.square, backgroundColor: color }} />
+                    ))}
+                </div>
+
+                <div style={{ ...styles.face, gridColumn: "1", gridRow: "2" }}>
+                    {cubeState.L.map((color, i) => (
+                        <div key={i} style={{ ...styles.square, backgroundColor: color }} />
+                    ))}
+                </div>
+
+                <div style={{ ...styles.face, gridColumn: "2", gridRow: "2" }}>
+                    {cubeState.F.map((color, i) => (
+                        <div key={i} style={{ ...styles.square, backgroundColor: color }} />
+                    ))}
+                </div>
+
+                <div style={{ ...styles.face, gridColumn: "3", gridRow: "2" }}>
+                    {cubeState.R.map((color, i) => (
+                        <div key={i} style={{ ...styles.square, backgroundColor: color }} />
+                    ))}
+                </div>
+
+                <div style={{ ...styles.face, gridColumn: "4", gridRow: "2" }}>
+                    {cubeState.B.map((color, i) => (
+                        <div key={i} style={{ ...styles.square, backgroundColor: color }} />
+                    ))}
+                </div>
+
+                <div style={{ ...styles.face, gridColumn: "2", gridRow: "3" }}>
+                    {cubeState.D.map((color, i) => (
+                        <div key={i} style={{ ...styles.square, backgroundColor: color }} />
+                    ))}
+                </div>
+
             </div>
         </div>
 
@@ -204,6 +396,27 @@ const styles = {
         alignItems: "center",
         gap: "10px",
         marginBottom: "5px",
+    },
+    cube: {
+        position: "absolute",
+        left: "40px",
+        bottom: "40px",
+        display: "grid",
+        gridTemplateColumns: "repeat(4, auto)",
+        gridTemplateRows: "repeat(3, auto)",
+        gap: "5px",
+    },
+
+    face: {
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 15px)",
+        gap: "2px",
+        marginBottom: "5px",
+    },
+
+    square: {
+        width: "15px",
+        height: "15px",
     },
 };
 
